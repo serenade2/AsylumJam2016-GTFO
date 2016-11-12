030cf40b78e59c6519885404a6ac43762b575558
WeirdUser <kevin.eric.bouchard@hotmail.com>
2016-11-12 12:29:32 -0500

    Trap update to UI.

--- Assets/scripts/UI/GameUI.cs
+++ Assets/scripts/UI/GameUI.cs
@@ -8,14 +8,30 @@ public class GameUI : MonoBehaviour {
 
     public Text timerText;
 
-	// Use this for initialization
-	void Start () {
-	
+    public Image trap1Image;
+    public int trap1Counter;
+    public Text trap1Text;
+
+    public Image trap2Image;
+    public int trap2Counter;
+    public Text trap2Text;
+
+    public Image trap3Image;
+    public int trap3Counter;
+    public Text trap3Text;
+
+    public Image trap4Image;
+    public int trap4Counter;
+    public Text trap4Text;
+
+    // Use this for initialization
+    void Start () {
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+        
         int minutes;
         int seconds;
 
@@ -25,6 +41,17 @@ public class GameUI : MonoBehaviour {
         seconds = (int)(timeLeftInSeconds % 60);
 
         timerText.text = minutes + " : " + seconds;
+
+    }
+
+    // OnGUI is called just like Update, once per frame
+    void OnGUI()
+    {
+        
+        trap1Text.text = " x " + trap1Counter;
+        trap2Text.text = " x " + trap2Counter;
+        trap3Text.text = " x " + trap3Counter;
+        trap4Text.text = " x " + trap4Counter;
 
     }
 }
