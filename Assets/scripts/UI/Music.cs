82035d3ea209b5d873afa36f9b32c4c10f8ee10b
WeirdUser <kevin.eric.bouchard@hotmail.com>
2016-11-12 13:30:30 -0500

    Music is functional. It is attached to the camera.

---
+++ Assets/scripts/UI/Music.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Music : MonoBehaviour {
+
+    AudioSource musicPlayer = new AudioSource();
+    public AudioClip song;
+
+	// Use this for initialization
+	void Start () {
+
+        musicPlayer.clip = song;
+        musicPlayer.loop = true;
+        musicPlayer.Play();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
