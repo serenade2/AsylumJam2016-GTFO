a29802ba116f3bb6e5c496443d9d42390aaf1f5a
Cédric Landry-Desjardins <Miniced_007@msn.com>
2016-11-12 15:51:34 -0500

    Added shaders and background

---
+++ Assets/scripts/CameraShading.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class CameraShading : MonoBehaviour
+{
+    public Material MaterialPostEffect;
+    private float _time;
+
+    void FixedUpdate()
+    {
+        _time += Time.fixedDeltaTime;
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        //Graphics.Blit(source, destination);
+        if (MaterialPostEffect != null)
+        {
+            MaterialPostEffect.SetFloat("__Time", _time);
+            Graphics.Blit(source, destination, MaterialPostEffect);
+        }
+        else
+            Graphics.Blit(source, destination);
+
+    }
+}
