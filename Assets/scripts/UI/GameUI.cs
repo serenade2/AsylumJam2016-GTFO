e72faaabd241f426bb6cbbd6ae17403ab6e01769
WeirdUser <kevin.eric.bouchard@hotmail.com>
2016-11-12 09:41:45 -0500

    Added main menu scene and a timer for the UI.

---
+++ Assets/scripts/UI/GameUI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour {
+
+    public float timeLeftInSeconds = 180.0f;
+
+    public Text timerText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        int minutes;
+        int seconds;
+
+        timeLeftInSeconds -= Time.deltaTime;
+
+        minutes = (int)(timeLeftInSeconds / 60);
+        seconds = (int)(timeLeftInSeconds % 60);
+
+        timerText.text = minutes + " : " + seconds;
+
+    }
+}
