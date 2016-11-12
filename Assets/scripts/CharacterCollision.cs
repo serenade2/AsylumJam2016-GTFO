ac5e73e7d8e8e2fdd30682b2aa02e7bdb7b046cc
serenade2 <samuel.abraham.1@ens.etsmtl.ca>
2016-11-12 15:26:15 -0500

    -Implemented the physics layers and tag

---
+++ Assets/scripts/CharacterCollision.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class CharacterCollision : MonoBehaviour
+{
+    private Door door;
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag.Equals("Floor"))
+        {
+            Debug.Log(String.Format("{0} collision entered in collision with {1}", this.gameObject.tag, coll.gameObject.tag));
+        }
+        else if (coll.gameObject.tag.Equals("Door"))
+        {
+            Debug.Log(String.Format("{0} collision entered in collision with {1}", this.gameObject.tag, coll.gameObject.tag));
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag.Equals("Floor"))
+        {
+            Debug.Log(String.Format("{0} entered in collision with {1}", this.gameObject.tag, coll.gameObject.tag));
+        }
+        else if (coll.gameObject.tag.Equals("Door"))
+        {
+            Debug.Log(String.Format("{0} entered in collision with {1}", this.gameObject.tag, coll.gameObject.tag));
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag.Equals("Floor"))
+        {
+            Debug.Log(String.Format("{0} trigger exited from collision with {1}", this.gameObject.tag, coll.gameObject.tag));
+        }
+        else if (coll.gameObject.tag.Equals("Door"))
+        {
+            Debug.Log(String.Format("{0} trigger exited from collision with {1}", this.gameObject.tag, coll.gameObject.tag));
+        }
+    }
+
+    void OpenDoor(GameObject doorGameObject)
+    {
+        door = doorGameObject.GetComponent<Door>();
+        if (door != null)
+        {
+            door.OpenDoor();
+        }
+    }
+}
