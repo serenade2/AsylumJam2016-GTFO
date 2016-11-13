b736d03bbe41205be5bca188c9354af1a7077f97
serenade2 <samuel.abraham.1@ens.etsmtl.ca>
2016-11-12 23:45:57 -0500

    -Begin of interaction

---
+++ Assets/scripts/Interaction.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interaction : MonoBehaviour
+{
+    private Transform _parentLookingDirection;
+    private float _offsetDistance;
+	// Use this for initialization
+	void Start ()
+	{
+	    _parentLookingDirection = GetComponentInParent<Transform>();
+        //_offsetDistance = 
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Debug.Log("Vertical axis " + Input.GetAxisRaw("Vertical"));
+	    float verticalAxis = Input.GetAxisRaw("Vertical");
+	    float horizontalAxis = Input.GetAxisRaw("Horizontal");
+
+	    if (verticalAxis == 0.0f)
+	    {
+	        //this.transform.position.x *=
+	    }
+        
+    }
+}
